7b568b5f47f681965b037379ac2129e5c9813e58
polymattic <matt@polymathic.me>
2016-01-19 19:44:01 -0600

    Deployment Config

--- config.rb
+++ config.rb
@@ -4,6 +4,16 @@ configure :development do
   activate :livereload
 end
 
+# GH-Pages deployment configuration
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # Add bower's directory to sprockets asset path
 after_configuration do
   sprockets.append_path File.join "#{root}", "node_modules"
@@ -17,14 +27,4 @@ configure :build do
 
   # activate :relative_assets
   # set :relative_links, true
-end
-
-# GH-Pages deploymentconfiguration
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
